ba4dcf3b2c58e1199c98749cbb2647cc33849f1f
I Putu Phillip Steven <putu.phillip@gmail.com>
2023-09-19 15:23:36 +0700

    remove readme, index, putu9, 10, 11, 12

--- putu11.js
+++
@@ -1,9 +0,0 @@
-function getDaysPassed() {
-  const date = new Date();
-  const thisYear = new Date(String(date.getFullYear()));
-  return `This year, days has passed are ${Math.floor(
-    (Date.parse(date) - Date.parse(thisYear)) / 86400000
-  )} days`;
-}
-
-console.log(getDaysPassed());
