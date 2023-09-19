8149047b8ee1a6e053507826ea8fd3989d6de64e
I Putu Phillip Steven <putu.phillip@gmail.com>
2023-09-19 10:12:50 +0700

    update putu9.js
    this function is to find odd element in the first half of array

--- putu9.js
+++ putu9.js
@@ -1,1 +1,15 @@
 // Number 9
+let array9 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+function oddHalf(arr) {
+  let newArr = [];
+  for (let i = 0; i < Math.floor(arr.length / 2); i++) {
+    console.log(arr[i]);
+    if (arr[i] % 2 !== 0) {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
+}
+
+console.log(oddHalf(array9));
